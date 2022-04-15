c2abe84f7fffc8f5e9df8a194e1937f42453029a
Gokhan T <trgtgokhan@gmail.com>
2022-04-15 23:13:59 +0300

    Change to using deployed server in the client

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import GlobalStyles from "./styles";
 import Pages from "./pages";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
+  uri: "https://odyssey-deployment.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
